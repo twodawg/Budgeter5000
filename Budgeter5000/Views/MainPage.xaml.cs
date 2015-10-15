545fe4e7ae750d13894a8f2bd31b25a040cf393c
Michael Twohey <mtwohey2@yahoo.com>
2015-10-15 05:38:45 -0700

    Now using NuGet Prism.Windows

--- Budgeter5000/Views/MainPage.xaml.cs
+++ Budgeter5000/Views/MainPage.xaml.cs
@@ -1,12 +1,12 @@
 ﻿using Prism.Windows.Mvvm;
-
+using Windows.UI.Xaml.Controls;
 
 namespace Budgeter5000.Views
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : VisualStateAwarePage
+    public sealed partial class MainPage : Page
     {
         public MainPage()
         {
--- Budgeter5000/Views/UserInputPage.xaml.cs
+++ Budgeter5000/Views/UserInputPage.xaml.cs
@@ -1,10 +1,11 @@
 ﻿using Prism.Windows.Mvvm;
+using Windows.UI.Xaml.Controls;
 
 // The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
 
 namespace Budgeter5000.Views
 {
-    public sealed partial class UserInputPage : VisualStateAwarePage
+    public sealed partial class UserInputPage : Page
     {
         public UserInputPage()
         {
