1c4eeed89ba9dee68570ee1613db33edef67e830
twodawg <mtwohey2@yahoo.com>
2015-10-24 09:13:04 -0700

    Interface

---
+++ Budget5000.Infrastructure/Interface/IAccountService.cs
@@ -0,0 +1,11 @@
+﻿using Budget5000.Infrastructure.Model;
+using System.Collections.Generic;
+
+namespace Budget5000.Infrastructure.Interface
+{
+    public interface IAccountService
+    {
+        List<Account> GetAccounts();
+        void AddAccount(Account account);
+    }
+}
