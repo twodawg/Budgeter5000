cb2b4cbe8971986eb1479cd09113fc76acb67560
Michael Twohey <mtwohey2@yahoo.com>
2015-11-21 21:31:37 -0800

    The start of the report pdf generation

---
+++ Budget5000.Report/Templates/IncomeReport.xsl
@@ -0,0 +1,10 @@
+﻿<?xml version="1.0" encoding="UTF-8" ?>
+<xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform"
+                xmlns:fo="http://www.w3.org/1999/XSL/Format"
+                xmlns:xalan="http://xml.apache.org/xalan"
+                xmlns:tns="tns:tns"
+                xmlns:ms="urn:schemas-microsoft-com:xslt">
+  <fo:root xmlns:fo="http://www.w3.org/1999/XSL/Format" font-family="Arial" font-size="8pt" line-height="1.2">
+    
+  </fo:root>
+</xsl:stylesheet>
--- Budget5000.Report/ViewModels/IncomeStatementViewModel.cs
+++ Budget5000.Report/ViewModels/IncomeStatementViewModel.cs
@@ -1,10 +1,53 @@
-﻿using Prism.Mvvm;
+﻿using System;
+using Prism.Commands;
+using Prism.Mvvm;
+using System.Xml.Xsl;
+using Fonet;
+using System.Diagnostics;
+using Fonet.Render.Pdf;
 
 namespace Budget5000.Report.ViewModels
 {
     public class IncomeStatementViewModel : BindableBase
     {
         public IncomeStatementViewModel()
-        { }
+        {
+            PrintIncomeReport = new DelegateCommand(OnPrintIncomeReport);
+        }
+
+        private void OnPrintIncomeReport()
+        {
+            var options = new PdfRendererOptions();
+            options.Author = "Michael Twohey";
+            options.Title = "Income Statement";
+            options.Subject = "Bugeter 5000";
+            //options.EnableModify = false;
+            //options.EnableAdd = false;
+            //options.EnableCopy = false;
+            //options.EnablePrinting = true;
+            //options.OwnerPassword = "slough";
+
+            string foxslPath = @"Templates\hello.fo";
+            //string foxslPath = @"Templates\IncomeReport.xsl";
+            string outputPath = "Report.pdf";
+
+            var driver = FonetDriver.Make();
+            driver.OnError += new FonetDriver.FonetEventHandler(FonetError);
+            driver.Options = options;
+            driver.Render(foxslPath, outputPath);
+
+            Process.Start(outputPath);
+        }
+
+        public DelegateCommand PrintIncomeReport { get; set; }
+
+        public void FonetError(object Driver, FonetEventArgs e)
+        {
+            const string LogName = "Application";
+            // Insert into Event Log
+            EventLog Log = new EventLog();
+            Log.Source = LogName;
+            Log.WriteEntry(e.GetMessage(), EventLogEntryType.Error);
+        }
     }
 }
