6c0b70c4b7006cfca62367c702922d29e7c1c7ac
twodawg <mtwohey2@yahoo.com>
2015-12-08 13:52:23 -0800

    Added a testing project and sample test

---
+++ Budget5000.Test/TransactionServiceTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Budget5000.Service.Service;
+using Budget5000.Infrastructure.Model;
+
+namespace Budget5000.Test
+{
+    [TestClass]
+    public class TransactionServiceTest
+    {
+        [TestMethod]
+        public void TransactionAdd()
+        {
+            var transactionService = new TransactionService();
+            var transData = transactionService.GetTransactions();
+            var transCount = transData.Count;
+
+            transData.Add(new Transaction()
+            {
+                Description = "Test Transaction",
+                TimeStamp = DateTime.Now,
+            });
+
+            Assert.AreEqual(++transCount, transData.Count);
+        }
+    }
+}
