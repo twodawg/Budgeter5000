0b37f9b39a55c340eb971d1ccbfb695c9fa5eef9
twodawg <mtwohey2@yahoo.com>
2015-12-24 11:32:02 -0800

    Messagebox infrastructure

---
+++ Budget5000.TransactionForm/ViewModels/IMessageBoxManager.cs
@@ -0,0 +1,9 @@
+﻿using System.Windows;
+
+namespace Budget5000.TransactionForm.ViewModels
+{
+    public interface IMessageBoxManager
+    {
+        MessageBoxResult ShowMessageBox(string text, string title, MessageBoxButton buttons);
+    }
+}
---
+++ Budget5000.TransactionForm/Views/MessageBoxView.cs
@@ -0,0 +1,7 @@
+﻿namespace Budget5000.TransactionForm.Views
+{
+    public class MessageBoxView
+    {
+        //http://stackoverflow.com/questions/10227007/are-you-sure-prompts-part-of-the-viewmodel-or-purely-the-view
+    }
+}
