7a1f9bac99968cc936645eea5b544efdc6911f8f
Michael Twohey <mtwohey2@yahoo.com>
2016-01-05 06:17:01 -0800

    Seperated DrawGraph methods from view model

---
+++ Budget5000.Graph/ViewModels/DrawGraph.cs
@@ -0,0 +1,80 @@
+﻿using Budget5000.Infrastructure.Model;
+using OxyPlot;
+using OxyPlot.Axes;
+using OxyPlot.Series;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Budget5000.Graph.ViewModels
+{
+    public static class DrawGraph
+    {
+        public static void Draw(PlotModel IncomePlot, ObservableCollection<Transaction> transactions)
+        {
+            ClearGraph(IncomePlot);
+
+            AddAxis(IncomePlot, transactions);
+
+            AddSeries(IncomePlot, transactions);
+
+            IncomePlot.InvalidatePlot(true);
+        }
+        static void ClearGraph(PlotModel IncomePlot)
+        {
+            IncomePlot.Axes.Clear();
+            IncomePlot.Series.Clear();
+            IncomePlot.Annotations.Clear();
+        }
+        static void AddAxis(PlotModel IncomePlot, ObservableCollection<Transaction> transactions)
+        {
+            var dateAxis = new DateTimeAxis
+            {
+                MajorGridlineStyle = LineStyle.Solid,
+                MinorGridlineStyle = LineStyle.Dot,
+                Angle = -45
+            };
+            IncomePlot.Axes.Add(dateAxis);
+            var valueAxis = new LinearAxis
+            {
+                MajorGridlineStyle = LineStyle.Solid,
+                MinorGridlineStyle = LineStyle.Dot,
+                Title = "Amount"
+            };
+            IncomePlot.Axes.Add(valueAxis);
+        }
+        static void AddSeries(PlotModel IncomePlot, ObservableCollection<Transaction> transactions)
+        {
+            // Income
+            foreach (var data in transactions.Where(q => q.AccountID >= 400 && q.AccountID < 500))
+            {
+                var barSeries = new LinearBarSeries
+                {
+                    StrokeThickness = 1,
+                    FillColor = OxyColors.Green,
+                    Title = data.Description
+                };
+                barSeries.Points.Add(new DataPoint(DateTimeAxis.ToDouble(data.TimeStamp),
+                    double.Parse(data.Amount.ToString())));
+                IncomePlot.Series.Add(barSeries);
+            }
+            // Expense
+            foreach (var data in transactions.Where(q => q.AccountID >= 500))
+            {
+                var barSeries = new LinearBarSeries
+                {
+                    StrokeThickness = 1,
+                    FillColor = OxyColors.IndianRed,
+                    Title = data.Description
+                };
+
+                barSeries.Points.Add(new DataPoint(DateTimeAxis.ToDouble(data.TimeStamp),
+                    double.Parse((-data.Amount).ToString())));
+                IncomePlot.Series.Add(barSeries);
+            }
+        }
+    }
+}
--- Budget5000.Graph/ViewModels/IncomeGraphViewModel.cs
+++ Budget5000.Graph/ViewModels/IncomeGraphViewModel.cs
@@ -2,11 +2,8 @@
 using Budget5000.Infrastructure.Model;
 using Budget5000.Service.Service;
 using OxyPlot;
-using OxyPlot.Axes;
-using OxyPlot.Series;
 using Prism.Mvvm;
 using System.Collections.ObjectModel;
-using System.Linq;
 
 namespace Budget5000.Graph.ViewModels
 {
@@ -21,86 +18,17 @@ namespace Budget5000.Graph.ViewModels
         }
         // Public Methods
 
-
         // Private Methods
         void Initalize()
         {
             IncomePlot = _GraphService.WorkingPlotModel;
             _TransactionService.Updated += _TransactionService_Updated;
 
-            DrawGraph(_TransactionService.WorkingTransactions);
+            DrawGraph.Draw(IncomePlot, _TransactionService.WorkingTransactions);
         }
-
         void _TransactionService_Updated(object sender, ObservableCollection<Transaction> e)
         {
-            DrawGraph(e);
-        }
-
-        void DrawGraph(ObservableCollection<Transaction> transactions)
-        {
-            ClearGraph();
-
-            AddAxis(transactions);
-
-            AddSeries(transactions);
-
-            IncomePlot.InvalidatePlot(true);
-        }
-
-        void AddSeries(ObservableCollection<Transaction> transactions)
-        {
-            // Income
-            foreach (var data in transactions.Where(q => q.AccountID >= 400 && q.AccountID < 500))
-            {
-                var barSeries = new LinearBarSeries
-                {
-                    StrokeThickness = 1,
-                    FillColor = OxyColors.Green,
-                    Title = data.Description
-                };
-                barSeries.Points.Add(new DataPoint(DateTimeAxis.ToDouble(data.TimeStamp),
-                    double.Parse(data.Amount.ToString())));
-                IncomePlot.Series.Add(barSeries);
-            }
-            // Expense
-            foreach (var data in transactions.Where(q => q.AccountID >= 500))
-            {
-                var barSeries = new LinearBarSeries
-                {
-                    StrokeThickness = 1,
-                    FillColor = OxyColors.IndianRed,
-                    Title = data.Description
-                };
-
-                barSeries.Points.Add(new DataPoint(DateTimeAxis.ToDouble(data.TimeStamp),
-                    double.Parse((-data.Amount).ToString())));
-                IncomePlot.Series.Add(barSeries);
-            }
-        }
-
-        void AddAxis(ObservableCollection<Transaction> transactions)
-        {
-            var dateAxis = new DateTimeAxis
-            {
-                MajorGridlineStyle = LineStyle.Solid,
-                MinorGridlineStyle = LineStyle.Dot,
-                Angle = -45
-            };
-            IncomePlot.Axes.Add(dateAxis);
-            var valueAxis = new LinearAxis
-            {
-                MajorGridlineStyle = LineStyle.Solid,
-                MinorGridlineStyle = LineStyle.Dot,
-                Title = "Amount"
-            };
-            IncomePlot.Axes.Add(valueAxis);
-        }
-
-        void ClearGraph()
-        {
-            IncomePlot.Axes.Clear();
-            IncomePlot.Series.Clear();
-            IncomePlot.Annotations.Clear();
+            DrawGraph.Draw(IncomePlot, e);
         }
 
         // Properties
